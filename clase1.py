55ef19d2c6cc5d8e7aeffe00178484facf31ac06
wladimir pineda <wladimir2162703@correo.uis.edu.co>
2021-01-31 13:11:52 -0500

    el ejercicio ha terminado

--- clase1.py
+++ clase1.py
@@ -108,10 +108,29 @@ if not success:
     raise Exception("MODFLOW 6 did not terminate normally.")
 
 
+#impriiendo resultados
+headfile= 'workspace'+'/'+ headfile
+hds = flopy.utils.binaryfile.HeadFile(headfile)
+h = hds.get_data(kstpkper=(0, 0))
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[0], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%2.1f")
+
+#Plotear una capa distinta
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[-1], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
 
 
-
-
-
-
-
+#plotear seccion transversal
+z = np.linspace(-H / Nlay / 2, -H + H / Nlay / 2, Nlay)
+fig = plt.figure(figsize=(5, 2.5))
+ax = fig.add_subplot(1, 1, 1, aspect="auto")
+c = ax.contour(x, z, h[:, 50, :], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
